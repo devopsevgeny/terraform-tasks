4c185aed2ab65d05b97225568d346d949a4f26e6
Evgeny Feigelman <devops.evgeny@gmail.com>
2025-06-25 23:36:19 +0300

    Addin files

---
+++ Task1/variables.tf
@@ -0,0 +1,35 @@
+variable "env" {
+  description = "Environment: dev or prod"
+  type        = string
+  default     = "dev"
+}
+
+variable "image_name" {
+  type = map(string)
+  default = {
+    dev  = "ghost:latest"
+    prod = "ghost:alpine"
+  }
+}
+
+variable "container_name" {
+  type = map(string)
+  default = {
+    dev  = "blog_dev"
+    prod = "blog_prod"
+  }
+}
+
+variable "int_port" {
+  type    = string
+  default = "2368"
+}
+
+variable "ext_port" {
+  type = map(string)
+  default = {
+    dev  = "8081"
+    prod = "8080"
+  }
+}
+
